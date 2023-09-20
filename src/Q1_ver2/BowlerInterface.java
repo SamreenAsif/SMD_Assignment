403a883c8bddd74059ee5f622fd958254b49ce3b
SamreenAsif <l201292@lhr.nu.edu.pk>
2023-09-20 09:58:24 +0500

    Q1 ver 2

---
+++ src/Q1_ver2/BowlerInterface.java
@@ -0,0 +1,5 @@
+package Q1_ver2;
+
+public interface BowlerInterface extends CricketerInterface{
+    int getRanking();
+}
---
+++ src/Q1_ver2/CricketerInterface.java
@@ -0,0 +1,6 @@
+package Q1_ver2;
+
+public interface CricketerInterface {
+    void Print();
+}
+
